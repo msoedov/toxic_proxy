afa282956b37d0eaaf238285fc8c7d433256030c
Alex Myasoedov <msoedov@gmail.com>
2017-12-03 15:51:36 -0500

    Test

---
+++ sideeffects.py
@@ -0,0 +1,21 @@
+import time
+from collections import defaultdict
+import asyncio
+
+trottling = defaultdict(int)
+
+async def lattency(**opts):
+    _latency = opts.get("latency")
+    await asyncio.sleep(_latency or 0)
+
+
+async def timeout(**opts):
+    await asyncio.sleep(10 * 60 * 60)
+
+
+async def bandwidth_rate_kb(**opts):
+    trottling[int(time.time())] += 2
+    sent = trottling[int(time.time())]
+    _bandwidth_rate_kb = opts.get("bandwidth_rate_kb")
+    if sent > _bandwidth_rate_kb:
+        await asyncio.sleep(1)
---
+++ test.py
@@ -0,0 +1,17 @@
+import asyncio
+from app import toxic_proxy
+
+
+proxy = toxic_proxy(destination=('google.com', 80), bandwidth_rate_kb=0.5, port=8888)
+loop = asyncio.get_event_loop()
+
+server = loop.run_until_complete(proxy)
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+# Close the server
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
