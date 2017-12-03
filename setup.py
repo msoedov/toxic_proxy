4dfe843131697536f4e7e68cf13e9b552c8e3746
Alex Myasoedov <msoedov@gmail.com>
2017-12-03 15:56:46 -0500

    Pip pack

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="toxic_proxy",
+    version="0.0.1",
+    description="An asyncio tcp proxy for network resiliency testing",
+    license="MIT",
+    author="Alex Myasoedov (Miasoiedov)",
+    author_email="msoedov+github@gmail.com",
+    packages=['.'],
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
